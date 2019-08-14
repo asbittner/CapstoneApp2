c70338e05b65a8fca0909d5b9500e9d89f6092f6
asbittner <42678277+asbittner@users.noreply.github.com>
2019-08-14 16:29:07 -0700

    Update app.py
    moved class to another file and imported

--- app.py
+++ app.py
@@ -98,27 +98,7 @@ def get_plot_dict(data, model):
             
     return county_plots
 
-    
-class stack_estimators(base.BaseEstimator, base.TransformerMixin):
-    
-    def __init__(self, estimator1, estimator2):
-        self.estimator1 = estimator1
-        self.estimator2 = estimator2
-        
-    def fit(self,X,y):
-        X2, y2 = ros.fit_resample(X, y) #balance
-        self.estimator1 = self.estimator1.fit(X2, y2)
-        
-        X3 = np.vstack((X.T, self.estimator1.predict(X))).T
-        X4, y4 = ros.fit_resample(X3,y)
-        
-        self.estimator2 = self.estimator2.fit(X4,y4)
-        
-        return self
-    
-    def predict(self, X):
-        X2 = np.vstack((X.T, self.estimator1.predict(X))).T
-        return self.estimator2.predict(X2)
+from .stack_estimators import stack_estimators
 
 class_data = pd.read_csv('static/class_data.csv')
 class_data['Pred'] = class_data['Risk Level']
