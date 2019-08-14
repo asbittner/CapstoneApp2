b7145f0e6e32eed035245fad4e5b3469df947dd1
asbittner <42678277+asbittner@users.noreply.github.com>
2019-08-14 16:29:43 -0700

    Add files via upload
    added stack estimators file

---
+++ stack_estimators.py
@@ -0,0 +1,20 @@
+class stack_estimators(base.BaseEstimator, base.TransformerMixin):
+    
+    def __init__(self, estimator1, estimator2):
+        self.estimator1 = estimator1
+        self.estimator2 = estimator2
+        
+    def fit(self,X,y):
+        X2, y2 = ros.fit_resample(X, y) #balance
+        self.estimator1 = self.estimator1.fit(X2, y2)
+        
+        X3 = np.vstack((X.T, self.estimator1.predict(X))).T
+        X4, y4 = ros.fit_resample(X3,y)
+        
+        self.estimator2 = self.estimator2.fit(X4,y4)
+        
+        return self
+    
+    def predict(self, X):
+        X2 = np.vstack((X.T, self.estimator1.predict(X))).T
+        return self.estimator2.predict(X2)
