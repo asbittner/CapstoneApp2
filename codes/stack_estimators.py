ecf3669f381e355c185e350f57b5b8bb39a28f66
asbittner <42678277+asbittner@users.noreply.github.com>
2019-08-14 17:23:28 -0700

    Add files via upload
    added codes

---
+++ codes/stack_estimators.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+import pickle
+import dill
+from sklearn import base
+
+class stack_estimators(base.BaseEstimator, base.TransformerMixin):
+    
+    def __init__(self, estimator1, estimator2):
+        self.estimator1 = estimator1
+        self.estimator2 = estimator2
+        
+    def fit(self,X,y):
+        X2, y2 = ros.fit_resample(X, y) #balance
+        self.estimator1 = self.estimator1.fit(X2, y2)
+        
+        X3 = np.vstack((X.T, self.estimator1.predict(X))).T
+        X4, y4 = ros.fit_resample(X3,y)
+        
+        self.estimator2 = self.estimator2.fit(X4,y4)
+        
+        return self
+    
+    def predict(self, X):
+        X2 = np.vstack((X.T, self.estimator1.predict(X))).T
+        return self.estimator2.predict(X2)
+        
+def LoadModel():
+    return pickle.load(open('static/finalized_model.sav', 'rb'))
